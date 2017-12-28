efdab699c3ddc68649653c19bcc3fafe1263c39f
hiteh <hubert.warzycha@gmail.com>
2017-12-28 20:22:06 +0100

    Add scripts and styles for navigation animations

--- src/js/app.js
+++ src/js/app.js
@@ -1,63 +1,67 @@
 import $ from "jquery";
+import velocity from 'velocity-animate';
 import '../scss/main.scss';
 
-function appInit(q,id) {
-  const domID = id||'app',
-        app = q(`#${domID}`),
-        listData = [
-          'JQuery',
-          'Sass/Autoprefixer',
-          'ExtractTextPlugin',
-          'Babel/ES6',
-          'Webpack',
-          'Webpack-Dev-Server'
-        ],
-        headerData = ':: JS Boilerplate ::';
+const handleOutlines = () => {
+  const buttons = $('button'),
+        links = $('a');
 
-  const createList = (data) => {
-    const list = q('<div>'),
-          wrapper = q('<div>'),
-          ul = q('<ul>'),
-          header = q('<h1>');
+  $(window).on('keydown', (event) => {
+    const key = event.keyCode;
 
-    list.addClass('list');
-    wrapper.addClass('wrapper');
-    header.text(headerData);
-    wrapper.append(header);
-
-    data.forEach((item) => {
-      let listItem = q('<li>');
-      listItem.text(item);
-      ul.append(listItem);
-    });
-
-    wrapper.append(ul);
-    list.append(wrapper);
-
-    return list;
-  }
-
-  const createHeader = (data) => {
-    const header = q('<h1>');
-    header.text(data);
-    return header;
-  }
-
-  const render = (args) => {
-    args.forEach((arg) => {
-      app.append(arg);
-    });
-  }
-
-  return {
-    renderView: () => {
-      const list = createList(listData);
-      render(
-        [list]
-      );
-    }
-  }
+    key === 9?
+    (buttons.addClass('outline'), links.addClass('outline')):
+    false;
+  });
+  $(window).on('click', (event) => {
+    buttons.hasClass('outline')?
+    (buttons.removeClass('outline'), links.removeClass('outline')):
+    false;
+  });
 }
 
-const App = appInit($);
-App.renderView();
+handleOutlines();
+
+const handleNavSlide = () => {
+  const nav = $('nav'),
+        button = $('.toggle-nav'),
+        buttonTopSpan = button.find('span:nth-child(1)'),
+        buttonMidSpan = button.find('span:nth-child(2)'),
+        buttonBottomSpan = button.find('span:nth-child(3)'),
+        spans = [buttonTopSpan,buttonMidSpan,buttonBottomSpan];
+
+  const animateButtonParts = () => {
+    buttonTopSpan.velocity({translateY: '8px',rotateZ: '135deg'},{duration: 300});
+    buttonMidSpan.velocity({translateX: '-40px',opacity: 0},{duration: 300});
+    buttonBottomSpan.velocity({translateY: '-8px', rotateZ: '-135deg'},{duration: 300});
+  }
+
+  const reverseAnimateButtonParts = () => {
+    spans.forEach((span) => {
+      span.velocity('reverse');
+    });
+  }
+
+  button.on('click', () => {
+    const navDisplay = nav.css('display');
+    const isVelocityAnimating = nav.hasClass('velocity-animating');
+
+    navDisplay === 'none' && !isVelocityAnimating?
+    (
+      button.attr('aria-pressed','true'),
+      button.addClass('nav-open'),
+      nav.attr('aria-expanded', 'true'),
+      animateButtonParts(),
+      nav.velocity('slideDown', {duration: 500, easing: 'ease-in'})
+    ):
+    (
+      button.attr('aria-pressed','false'),
+      button.removeClass('nav-open'),
+      nav.attr('aria-expanded', 'false'),
+      reverseAnimateButtonParts(),
+      nav.velocity('slideUp', {duration: 500, easing: 'ease-out'})
+    );
+  });
+}
+
+handleNavSlide();
