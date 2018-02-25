8cc763797f26b40ba17ec7e66c5a4d8ec82d2402
hiteh <hubert.warzycha@gmail.com>
2018-02-25 21:36:24 +0100

    Fix styles

--- src/js/app/modules/charts.js
+++ src/js/app/modules/charts.js
@@ -17,11 +17,11 @@ export default class Charts extends Module {
           data: {
               labels: ["January", "February", "March", "April", "May", "June", "July"],
               datasets: [{
-                  label: '# of Votes',
+                  label: 'Income',
                   data: [4, 12, 9, 10, 15, 18, 25],
-                  backgroundColor: 'rgba(0, 0, 0, 0.6)',
-                  borderColor: 'rgba(0, 0, 0, 1)',
-                  borderWidth: 1,
+                  backgroundColor: 'rgba(35,52,52,.8)',
+                  borderColor: 'rgba(255,182,71,1)',
+                  borderWidth: 2,
                   lineTension: 0
               }]
           },
@@ -42,11 +42,11 @@ export default class Charts extends Module {
             data: {
                 labels: ["January", "February", "March", "April", "May", "June", "July"],
                 datasets: [{
-                    label: '# of Votes',
+                    label: 'Costs',
                     data: [13, 16, 10, 9, 12, 8, 6],
-                    backgroundColor: 'rgba(0, 0, 0, 0.6)',
-                    borderColor: 'rgba(0, 0, 0, 1)',
-                    borderWidth: 1,
+                    backgroundColor: 'rgba(35,52,52,.8)',
+                    borderColor: 'rgba(255,182,71,1)',
+                    borderWidth: 2,
                     lineTension: 0
                 }]
             },
@@ -67,7 +67,13 @@ export default class Charts extends Module {
       data: {
         labels: ["Construction", "Know-how", "Consulting", "Inovations"],
         datasets: [{
-          data: [10, 20, 30, 40]
+          data: [10, 20, 30, 40],
+          backgroundColor: [
+               'rgba(0,102,204,.8)',
+               'rgba(204,0,0,.8)',
+               'rgba(255,182,71,.8)',
+               'rgba(35,52,52,.8)',
+           ],
         }],
       },
       options: {
