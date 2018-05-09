4b7dc91ba143b44cc78a0d40372c3a29a95be7d8
hiteh <hubert.warzycha@gmail.com>
2018-05-09 17:55:25 +0200

    Add bundle split and eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,62 +1,69 @@
-const path = require("path");
-const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require('path')
+const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const ExtractPlugin = new ExtractTextPlugin({
-    filename: 'css/style.css'
-});
+  filename: 'css/style.css'
+})
 
 module.exports = {
   entry: ['./src/js/index.js'],
   output: {
-    path: path.resolve(__dirname,'dist/'),
-    filename: 'js/bundle.js',
-    publicPath: '/',
+    path: path.resolve(__dirname, 'dist/'),
+    filename: 'js/[name].bundle.js',
+    publicPath: '/'
   },
   module: {
     rules: [
       {
-        test : /\.js$/,
-        exclude : /node_modules/,
+        test: /\.js$/,
+        exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           options: {
-            presets: 'env',
-          },
-        },
+            presets: 'env'
+          }
+        }
       },
+      {loader: 'eslint-loader'},
       {
         test: /\.scss$/,
         use: ExtractPlugin.extract({
           use: [
-          {loader: 'css-loader',options:{importLoaders: 1}},
-          'postcss-loader',
-          {loader: 'sass-loader',options: { precision: 8 }}
-        ]
+            {loader: 'css-loader', options: {importLoaders: 1}},
+            'postcss-loader',
+            {loader: 'sass-loader', options: {precision: 8}}
+          ]
         })
       },
       {
         test: /\.css$/,
         use: [
-        {loader: 'style-loader'},
-        {loader: 'css-loader',options:{importLoaders: 1}},
-        'postcss-loader',
+          {loader: 'style-loader'},
+          {loader: 'css-loader', options: {importLoaders: 1}},
+          'postcss-loader'
         ]
       },
       {
         test: /\.(png|jpg|gif|svg)$/,
         use: {
           loader: 'url-loader',
-          options: {limit: 10000, outputPath: 'img/',publicPath:'../img'},
+          options: {limit: 10000, outputPath: 'img/', publicPath: '../img'}
         }
       },
       {
         test: /\.(eot|ttf|woff|woff2)$/,
         use: {
           loader: 'url-loader',
-          options: {limit: 10000, outputPath: 'fonts/',publicPath:'../fonts'},
+          options: {limit: 10000, outputPath: 'fonts/', publicPath: '../fonts'}
         }
-      },
+      }
     ]
   },
-  plugins: [ExtractPlugin, new webpack.ProvidePlugin({$: "jquery", jQuery: "jquery", "window.jQuery": "jquery"})]
-};
+  plugins: [ExtractPlugin,
+    new webpack.ProvidePlugin({$: 'jquery', jQuery: 'jquery', 'window.jQuery': 'jquery'}),
+    new webpack.optimize.AggressiveSplittingPlugin({
+      minSize: 20000,
+      maxSize: 500000
+    })
+  ]
+}
